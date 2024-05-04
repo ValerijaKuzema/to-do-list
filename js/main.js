aea4b0d8af191da2271f6e38fdc418f0999e1dd9
Valerija Kuzema <valerija_kuzema@web.de>
2024-05-04 12:03:33 +0200

    some more Java Script to add a new li-Element

--- js/main.js
+++ js/main.js
@@ -3,15 +3,32 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const addButton = document.querySelector(".add-new-todo-btn");
-  const inputField = document.querySelector(".input-field-todo")
+  const inputField = document.querySelector(".input-field-todo");
+  const ulElement = document.querySelector("ul.list-group");
 
-  addButton.addEventListener("click",() => {
+  addButton.addEventListener("click", (event) => {
+    event.preventDefault;
     console.log("button was clicked!");
-  })
+    const text = inputField.value;
+    console.log(text);
 
-  inputField.addEventListener("keyup", () => {
-    const newToDo = inputField.value;
-    console.log(newToDo)
+    if (text === "") {
+      return
+    }
+
+    let newliElement = document.createElement("li");
+    newliElement.classList.add("list-element")
+    console.log(newliElement);
+
+    // let newCheckbox = document.createElement("input");
+    // newliElement.appendChild(newCheckbox);
+
+    newliElement.innerText = text;
+
+    const ulElement = document.querySelector("ul.list-group");
+    ulElement.appendChild(newliElement);
+
+    inputField.value = "";
   })
 
 });
