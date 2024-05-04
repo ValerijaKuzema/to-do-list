745a795f8cedb1d9dde9d8e824c913a0a157a17b
Valerija Kuzema <valerija_kuzema@web.de>
2024-05-04 11:23:05 +0200

    added checkbox,started to add some Java Script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 "use strict"
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const addButton = document.querySelector(".add-new-todo-btn");
+  const inputField = document.querySelector(".input-field-todo")
+
+  addButton.addEventListener("click",() => {
+    console.log("button was clicked!");
+  })
+
+  inputField.addEventListener("keyup", () => {
+    const newToDo = inputField.value;
+    console.log(newToDo)
+  })
+
+});
