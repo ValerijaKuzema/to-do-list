61ac82f8939114918d0343f683225ba605e2c6cf
Valerija Kuzema <valerija_kuzema@web.de>
2024-05-05 14:00:28 +0200

    added the new task adding in JavaScript

--- js/main.js
+++ js/main.js
@@ -4,31 +4,54 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const addButton = document.querySelector(".add-new-todo-btn");
   const inputField = document.querySelector(".input-field-todo");
-  const ulElement = document.querySelector("ul.list-group");
+  const addToTrash = document.querySelector(".destroy");
 
   addButton.addEventListener("click", (event) => {
-    event.preventDefault;
-    console.log("button was clicked!");
-    const text = inputField.value;
-    console.log(text);
+    event.preventDefault();
 
+    const text = inputField.value;
     if (text === "") {
       return
     }
 
+    const newButtonElement = document.createElement("button");
+    newButtonElement.classList.add("destroy");
+
+    const newLabelElement = document.createElement("label");
+    newLabelElement.innerText = text;
+
+    const newCheckboxElement = document.createElement("input");
+    newCheckboxElement.type = "checkbox";
+    newCheckboxElement.classList.add("toggle");
+
+    const newDivElement = document.createElement("div");
+
+    newDivElement.appendChild(newCheckboxElement);
+    newDivElement.appendChild(newLabelElement);
+    newDivElement.appendChild(newButtonElement);
+
     let newliElement = document.createElement("li");
-    newliElement.classList.add("list-element")
-    console.log(newliElement);
+    newliElement.classList.add("list-element");
+    newliElement.appendChild(newDivElement);
 
-    // let newCheckbox = document.createElement("input");
-    // newliElement.appendChild(newCheckbox);
-
-    newliElement.innerText = text;
-
-    const ulElement = document.querySelector("ul.list-group");
-    ulElement.appendChild(newliElement);
+    const ulElement = document.querySelector(".list-group");
+    ulElement.prepend(newliElement);
 
     inputField.value = "";
+  })
+
+  const checkbox = document.querySelector(".checkbox");
+
+  if (checkbox.classList.checked) {
+    checkbox.classList.add("background-color-task");
+  }
+
+
+  addToTrash.addEventListener("click", () => {
+  //   const removeLiElement = document.getElementsByClassName("list-element");
+  //   removeLiElement.remove;
+
+      console.log("trash was clicked");
   })
 
 });
